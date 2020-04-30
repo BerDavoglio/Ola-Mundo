b1809cef577597d028165006e5c8134c195cc96e
Bernardo <berdavoglio@gmail.com>
2020-04-30 01:37:58 -0300

    Update no código do arquivo .Py

--- Teste_CSharp/texte.py
+++ Teste_CSharp/texte.py
@@ -1,3 +1,23 @@
 print("Teste 1 no VSCode, ainda o PyCharm é melhor")
 print("Não consegui fazer o C# rodar aqui...")
 print("Provavelmente vou continuar usando o VSC 2019.")
+
+a = int(input('Digite um número inteiro: '))
+b = int(input('Digite outro valor inteiro: '))
+
+c = a + b
+
+print('A soma de {} + {} = {}'.format(a, b, c))
+
+soma = 0
+while True:
+    if soma >=3 42:
+        break
+    else:
+        x = int(input('Digite X = '))
+        soma += x
+        if soma >= 42:
+            break
+        print(soma)
+    print('Digite outro número!')
+print('Você conseguiu!')
